80c2c5cc1bfa657a1e61de755a8b1f263ed737ad
Sahil Vartak <sahilravu213@gmail.com>
2021-04-10 10:57:21 +0530

    Image Flip added

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ function display(){
     }
 
 }
+
+
+var link_container = document.getElementsByClassName("navbar");
+
+var btns = document.getElementsByClassName("anchor");
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+function mousein(){
+    console.log("Hello");
+    $('#profile').css('opacity', "5%");
+    $('#undder-img').css('display', 'flex');
+}
+
+function mouseout(){
+    $('#profile').css('opacity', "100%");
+    $('#undder-img').css('display', 'none');
+}
+
+$('#profile').hover(mousein, mouseout);
