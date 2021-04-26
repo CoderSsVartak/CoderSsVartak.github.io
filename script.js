101cc270e8e0950720203dedb3eb72cd7de5f38e
Sahil Vartak <sahilravu213@gmail.com>
2021-04-26 12:44:40 +0530

    Further optimized for faster reloads

--- script.js
+++ script.js
@@ -3,14 +3,21 @@ var display_flip = "shown";
 
 function display(){
     
-    document.getElementById('mobile-navigation').style.transitionProperty = "ease-in-out 2s";
     document.getElementById('mobile-navbar').style.borderRadius = "15px";
 
     if (document.getElementById('mobile-navigation').style.display == "flex"){
+        document.getElementById('menu-img').style.transform = "rotateZ(180deg)";
+        document.getElementById('menu-img').style.transition = "linear .4s";
+        document.getElementById('menu-img').style.transition = "linear .4s";
+        document.getElementById('menu-img').src = "media/hamburger.jpg";
         document.getElementById('mobile-navigation').style.display = "none";
     }
     else{
         document.getElementById('mobile-navigation').style.display = "flex";
+        document.getElementById('menu-img').style.transform = "rotateZ(0deg)";
+        document.getElementById('menu-img').style.transition = "linear .4s";
+        document.getElementById('menu-img').style.borderRadius = "19px";
+        document.getElementById('menu-img').src = "media/close.png";
     }
 
 }
@@ -33,11 +40,15 @@ for (var i = 0; i < btns.length; i++) {
 function hide_image(){
 
     if(display_flip == "shown"){
+        document.getElementById('profile').style.transform = "rotateY(180deg)"
+        document.getElementById('profile').style.transition = "linear .4s"
         $('#profile').css('opacity', "0%");
         $('#undder-img').css('display', 'flex');
         display_flip = "hidden";
     }
     else{
+        document.getElementById('profile').style.transform = "rotateY(0deg)"
+        document.getElementById('profile').style.transition = "linear .4s"
         $('#profile').css('opacity', "100%");
         $('#undder-img').css('display', 'none');
         display_flip = "shown";
