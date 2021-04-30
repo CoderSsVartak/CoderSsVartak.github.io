d89f83a41fd59df858b223281803a86a3ea02fdf
Sahil Vartak <sahilravu213@gmail.com>
2021-04-30 18:51:56 +0530

    Updated UI

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ var display_flip = "shown";
 function display(){
     
     
-    document.getElementById('mobile-navbar').style.borderRadius = "15px";
 
     if (document.getElementById('mobile-navigation').style.display == "flex"){
         document.getElementById('menu-img').style.transform = "rotateZ(0deg)";
-        document.getElementById('menu-img').style.transition = "linear .4s";
         document.getElementById('menu-img').style.transition = "linear .4s";
         document.getElementById('menu-img').src = "media/hamburger.jpg";
         document.getElementById('mobile-navigation').style.display = "none";
@@ -18,6 +16,7 @@ function display(){
         document.getElementById('menu-img').style.transform = "rotateZ(180deg)";
         document.getElementById('menu-img').style.transition = "linear .4s";
         document.getElementById('menu-img').style.borderRadius = "19px";
+        document.getElementById('menu-img').style.backgroundColor = "lightblue";
         document.getElementById('menu-img').src = "media/close.png";
     }
 
@@ -37,19 +36,22 @@ for (var i = 0; i < btns.length; i++) {
     });
 }
 
+
 // Profile image flip
 function hide_image(){
 
     if(display_flip == "shown"){
-        document.getElementById('profile').style.transform = "rotateY(180deg)"
-        document.getElementById('profile').style.transition = "linear .4s"
+        document.getElementById('profile').style.transform = "rotateY(180deg)";
+        document.getElementById('indicator').style.display = "none";
+        document.getElementById('profile').style.transition = "linear .6s";
         $('#profile').css('opacity', "0%");
         $('#undder-img').css('display', 'flex');
         display_flip = "hidden";
     }
     else{
-        document.getElementById('profile').style.transform = "rotateY(0deg)"
-        document.getElementById('profile').style.transition = "linear .4s"
+        document.getElementById('profile').style.transform = "rotateY(0deg)";
+        document.getElementById('indicator').style.display = "inline-flex";
+        document.getElementById('profile').style.transition = "linear .4s";
         $('#profile').css('opacity', "100%");
         $('#undder-img').css('display', 'none');
         display_flip = "shown";
